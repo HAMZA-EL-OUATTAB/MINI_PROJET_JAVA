a3796157ca5c14232a91ae42dc211fac22266739
pc <pc@HAMZA-LAPTOP>
2024-11-20 11:52:42 +0100

    Interface Notes d'etudiant

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,37 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Notes.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 886,800);
+
+            scene.getStylesheets().add(getClass().getResource("/View/application.css").toExternalForm());
+            primaryStage.setTitle("Gestionnaire de Notes"); // Ajout du titre
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            // Affichage d'une alerte en cas d'erreur
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText("Impossible de charger la vue");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
